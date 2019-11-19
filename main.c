0602f09dc02cab6fc1e55d92142705df2313e806
kang-haneul <55428030+kang-haneul@users.noreply.github.com>
2019-11-19 15:36:08 +0900

    04

--- main.c
+++ main.c
@@ -4,15 +4,12 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 	
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
+	char str[30] = "happy C programming";
 	
-	strcpy(dst,src);
+	printf("string %s of length : %i",str,strlen(str));
 	
-	printf("copied string : %s",dst);
-	
-	
+return 0;
 	
 }
